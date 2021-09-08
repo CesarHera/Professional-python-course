cae96a0340293b9c9ad04e5945eeef12fe6f61a7
Cesar Hera <cesar.hr2003@gmail.com>
2021-09-07 19:30:48 -0500

    Base file of the funcition prime_finder

---
+++ static_typing.py
@@ -0,0 +1,22 @@
+def prime_detector(number: int) -> bool:
+    if number == 1:
+        return False
+    if number == 2:
+        return True
+    elif number < 1:
+        return False
+
+    for i in range(1, number):
+        if i == 1:
+            continue
+        elif number % i == 0:
+            return False
+    return True
+
+
+def run():
+    print([i for i in range(0, 100) if prime_detector(i)])
+
+
+if __name__ == '__main__':
+    run()
